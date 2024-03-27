f56e2c7d40023899abf2b7d33635fc18a848aaf1
Zafir <shamsizafir@gmail.com>
2024-03-26 22:34:47 -0400

    m

--- main.py
+++ main.py
@@ -18,6 +18,11 @@ def home():
     with col2:
         st.markdown("<p style='text-align: center;'>Ticker 2</p>", unsafe_allow_html=True)
         ticker2 = st.text_input("", value='', max_chars=None, key=None, type='default', help=None, placeholder="Ex: MSFT", on_change=None, args=None, kwargs=None)
+        
+    # Dropdown for selecting time frame
+    st.markdown("<h3 style='text-align: center;'>Select Time Frame</h3>", unsafe_allow_html=True)
+    time_frames = ["1D", "1W", "3M", "6M", "1Y", "All"]
+    selected_time_frame = st.selectbox("", options=time_frames, index=0, format_func=lambda x: x)
 
 def about():
     # About page content
