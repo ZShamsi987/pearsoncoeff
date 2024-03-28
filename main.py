215abcce80f249919cbb7ac1a06f3da628857eba
Zafir <shamsizafir@gmail.com>
2024-03-27 22:43:20 -0400

    m

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def home():
 
     # Dropdown for selecting time frame
     st.markdown("<h3 style='text-align: center;'>Select Time Frame</h3>", unsafe_allow_html=True)
-    time_frames = ["1wk", "5d", "1mo", "3mo", "6mo", "1y", "ytd", "5y", "max"]
+    time_frames = ["1d", "5d", "1mo", "3mo", "6mo", "1y", "ytd", "5y", "max"]
     selected_time_frame = st.selectbox("", options=time_frames, index=0, format_func=lambda x: x)
 
     st.session_state.button_width = 200 
