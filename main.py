067ef15a3256d492f2c58118d618dfe1243e9d63
Zafir <shamsizafir@gmail.com>
2024-03-28 15:17:47 -0400

    m

--- main.py
+++ main.py
@@ -94,9 +94,6 @@ def about():
     st.write("-Custom time range")
     st.write("-Other statistical/quant elements")
 
-    st.write("")
-    st.write("")
-
     st.subheader("This app was written by [Zafir Shamsi](https://github.com/ZShamsi987)")
 
     st.write("")
